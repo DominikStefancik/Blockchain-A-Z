1771d3f69160c2e367dd7e2a836ef3fdf0158a51
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-07-02 18:41:11 +0200

    Add method for creating a block

--- Part-1-Create-Blockchain/blockchain.py
+++ Part-1-Create-Blockchain/blockchain.py
@@ -15,6 +15,13 @@ class Blockchain:
     
     def __init__(self):
         self.chain = [] # represents a chain of blocks
+        self.create_block(proof = 1, previous_hash = "0") # creates a genesis block
 
-
+    def create_block(self, proof, previous_hash):
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": str(datetime.datetime.now()), # timestamp when the block was mined
+            "proof": proof,
+            "previous_hash": previous_hash
+        }
 
