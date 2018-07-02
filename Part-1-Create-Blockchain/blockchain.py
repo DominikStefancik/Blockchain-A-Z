c11c0e5519bc4af2606e910da69714735c5916ca
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-07-02 21:29:44 +0200

    Add method for chain validation

--- Part-1-Create-Blockchain/blockchain.py
+++ Part-1-Create-Blockchain/blockchain.py
@@ -35,15 +35,44 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while not check_proof:
-            asymetrical_operation = new_proof**2 - previous_proof**2
-            # the argument of the function here has to be a non-symetrical operation
-            # e.g. new_proof + previous_proof cannot be used, because it is a symetrical operation
-            # encoding is required for sha256 method
-            hash_operation = hashlib.sha256(str(asymetrical_operation).encode()).hexdigest()
-            if hash_operation[:4] == "0000":
+            hash = self.get_hash_sha256(previous_proof, new_proof)
+            if self.is_hash_valid(hash):
                 check_proof = True
             else:
                 new_proof += 1
                 
         return new_proof        
 
+    def get_block_hash(self, block):
+        # we have to encode the block for sha256 function
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self):
+        for index, block in enumerate(self.chain[1:]):
+            previous_block = self.chain[index - 1]
+            
+            # check of previous hash matches
+            if block["previous_hash"] != self.hash(previous_block):
+                return False
+            
+            previous_proof = previous_block["proof"]
+            current_proof = block["proof"]
+            hash = self.get_hash_sha256(previous_proof, current_proof)
+            # check if proof of work is valid
+            if not self.is_hash_valid(hash):
+                return False
+            
+        return True
+    
+    def get_hash_sha256(self, previous_proof, current_proof):
+        asymetrical_operation = current_proof**2 - previous_proof**2
+        # the argument of the function here has to be a non-symetrical operation
+        # e.g. new_proof + previous_proof cannot be used, because it is a symetrical operation
+        # encoding is required for sha256 method
+        return hashlib.sha256(str(asymetrical_operation).encode()).hexdigest()
+    
+    def is_hash_valid(hash):
+        return hash[:4] == "0000"
+        
+        
